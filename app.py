f10f96e3d2d7ea90e46065876c25693f9445650e
karan <karanmatiyali123@gmail.com>
2024-04-16 00:41:36 +0530

    deployment purpose

--- app.py
+++ app.py
@@ -1,23 +1,68 @@
 from flask import Flask, render_template, request, jsonify
 from chat import get_response
 import csv
+import json
+from dotenv import load_dotenv
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
 app = Flask(__name__)
+load_dotenv()
+# Access environment variables
+apiKey = os.getenv("apiKey")
+authDomain = os.getenv("authDomain")
+projectId = os.getenv("projectId")
+storageBucket = os.getenv("storageBucket")
+messagingSenderId = os.getenv("messagingSenderId")
+appId = os.getenv("appId")
+
+type = os.getenv("type")
+project_id = os.getenv("project_id")
+private_key_id = os.getenv("private_key_id")
+private_key = os.getenv("private_key")
+client_email = os.getenv("client_email")
+client_id = os.getenv("client_id")
+auth_uri = os.getenv("auth_uri")
+token_uri = os.getenv("token_uri")
+auth_provider_x509_cert_url = os.getenv("auth_provider_x509_cert_url")
+client_x509_cert_url = os.getenv("client_x509_cert_url")
+universe_domain = os.getenv("universe_domain")
+
+json_data=r'''
+{{
+  "type": "{type}",
+  "project_id": "{project_id}",
+  "private_key_id": "{private_key_id}",
+  "private_key": "{private_key}",
+  "client_email": "{client_email}",
+  "client_id": "{client_id}",
+  "auth_uri": "{auth_uri}",
+  "token_uri": "{token_uri}",
+  "auth_provider_x509_cert_url": "{auth_provider_x509_cert_url}",
+  "client_x509_cert_url": "{client_x509_cert_url}",
+  "universe_domain": "{universe_domain}"
+}}
+'''.format(type=type,project_id=project_id,private_key_id=private_key_id,private_key=private_key,client_email=client_email,client_id=client_id,auth_uri=auth_uri,token_uri=token_uri,auth_provider_x509_cert_url=auth_provider_x509_cert_url,client_x509_cert_url=client_x509_cert_url,universe_domain=universe_domain)
+
+
+data = json.loads(json_data)
+with open('serviceAccountKey.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
 
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    "apiKey": "",
-    "authDomain": "",
-    "projectId": "",
-    "storageBucket": "",
-    "messagingSenderId": "",
-    "appId": ""
+    "apiKey": apiKey,
+    "authDomain": authDomain,
+    "projectId": projectId,
+    "storageBucket": storageBucket,
+    "messagingSenderId": messagingSenderId,
+    "appId": appId
 })
 db = firestore.client()
 variable=1
+
 
 
 @app.route('/')
@@ -75,5 +120,6 @@ def login():
 
 
 #commented for deployment purpose
-if __name__=='__main__':
-    app.run(debug=True)
+#remove comment of want to run in localhost
+# if __name__=='__main__':
+#     app.run(debug=True)
